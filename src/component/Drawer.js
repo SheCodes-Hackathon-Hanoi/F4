9f030ba1f5a2f83c1423ee4af5a00669ab5f1810
gitmphuong <phuongnguyen2522003@gmail.com>
2023-09-30 21:12:51 +0700

    commit

--- src/component/Drawer.js
+++ src/component/Drawer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -21,16 +21,59 @@ const PermanentDrawer = () => {
     fontSize: "50px",
   };
 
+  const initialButtonState = {
+    home: false,
+    accountInfor: false,
+    createTeam: false,
+    joinTeam: false,
+    logout: false,
+  };
+
+  const [buttonState, setButtonState] = useState(initialButtonState);
+
   const buttonStyle = {
     width: "200px",
     margin: "15px",
-    backgroundColor: "#E1F0F8",
     borderRadius: "10px",
     padding: "16px",
     color: "black",
   };
 
+  const redButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: "#F87C83",
+  };
+
   const navigate = useNavigate();
+
+  const handleButtonClick = (buttonName) => {
+    // Reset all button states to false
+    const updatedButtonState = { ...initialButtonState };
+    // Set the clicked button state to true
+    updatedButtonState[buttonName] = true;
+    setButtonState(updatedButtonState);
+
+    // Navigate to the appropriate page
+    switch (buttonName) {
+      case "home":
+        navigate("/HomePage");
+        break;
+      case "accountInfor":
+        navigate("/AccountInfor");
+        break;
+      case "createTeam":
+        navigate("/CreateTeam");
+        break;
+      case "joinTeam":
+        navigate("/JoinTeam");
+        break;
+      case "logout":
+        // Handle logout logic here
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div>
@@ -41,46 +84,43 @@ const PermanentDrawer = () => {
           <ul className="drawer-content">
             <li>
               <Button
-                style={buttonStyle}
-                onClick={() => {
-                  navigate("/HomePage");
-                }}
+                style={buttonState.home ? redButtonStyle : buttonStyle}
+                onClick={() => handleButtonClick("home")}
               >
                 Trang chủ
               </Button>
             </li>
             <li>
               <Button
-                style={buttonStyle}
-                onClick={() => {
-                  navigate("/AccountInfor");
-                }}
+                style={buttonState.accountInfor ? redButtonStyle : buttonStyle}
+                onClick={() => handleButtonClick("accountInfor")}
               >
                 Thông tin tài khoản
               </Button>
             </li>
             <li>
               <Button
-                style={buttonStyle}
-                onClick={() => {
-                  navigate("/CreateTeam");
-                }}
+                style={buttonState.createTeam ? redButtonStyle : buttonStyle}
+                onClick={() => handleButtonClick("createTeam")}
               >
                 Tạo nhóm
               </Button>
             </li>
             <li>
               <Button
-                style={buttonStyle}
-                onClick={() => {
-                  navigate("/JoinTeam");
-                }}
+                style={buttonState.joinTeam ? redButtonStyle : buttonStyle}
+                onClick={() => handleButtonClick("joinTeam")}
               >
                 Tham gia bằng mã
               </Button>
             </li>
             <li>
-              <Button style={buttonStyle}>Đăng xuất</Button>
+              <Button
+                style={buttonState.logout ? redButtonStyle : buttonStyle}
+                onClick={() => handleButtonClick("logout")}
+              >
+                Đăng xuất
+              </Button>
             </li>
           </ul>
         </div>
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div className='Header'>
-        Nguyễn Thị Hà Giang - Đại học Bách Khoa Hà Nội
+    <div className="Header" style={{ display: "flex" }}>
+      <div>Nguyễn Thị Hà Giang</div>
+      <div style={{ marginLeft: "450px" }}>Đại học Bách Khoa Hà Nội</div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
